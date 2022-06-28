dbb94669ba95e1ef8a01e4c125f3e00e5d6a20de
Ashish S <ashishjsharda@gmail.com>
2022-06-28 06:20:20 -0500

    Update node.py

--- node.py
+++ node.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 def main(args=None):
     rclpy.init(args=args)
-    node = Node('my_node_name')
+    node = Node('Hello')
     rclpy.spin(node)
     rclpy.shutdown()
 if __name__ == '__main__':
